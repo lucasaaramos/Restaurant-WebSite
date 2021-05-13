1f0d77d78aeb0598e09d1635241677f8c3136d89
ramoslucas <lucasalbuquerque5412@gmail.com>
2021-05-13 18:28:53 +0100

    JS login access

---
+++ js/script.js
@@ -0,0 +1,60 @@
+var myInput = document.getElementById("credit");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+
+    // Validate lowercase
+    var numbers = /[a-z]/g;
+    if(myInput.value.match(numbers)) {  
+        lowercase.classList.remove("invalid");
+        lowercase.classList.add("valid");
+    } else {
+        lowercase.classList.remove("valid");
+        lowercase.classList.add("invalid");
+    }
+
+    // Validate upercase
+    var numbers = /[A-Z]/g;
+    if(myInput.value.match(numbers)) {  
+        upercase.classList.remove("invalid");
+        upercase.classList.add("valid");
+    } else {
+        upercase.classList.remove("valid");
+        upercase.classList.add("invalid");
+    }
+
+    // Validate special caracteres 
+    var numbers = /(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})/g;
+    if(myInput.value.match(numbers)) {  
+        special.classList.remove("invalid");
+        special.classList.add("valid");
+    } else {
+        special.classList.remove("valid");
+        special.classList.add("invalid");
+    }
+
+        
+    // Validate number of caracteres
+    var numbers = /([^]){8}/g;
+    if(myInput.value.match(numbers)) {  
+        caracterNumb.classList.remove("invalid");
+        caracterNumb.classList.add("valid");
+    } else {
+        caracterNumb.classList.remove("valid");
+        caracterNumb.classList.add("invalid");
+    }
+ 
+
+}
