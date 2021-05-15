b399b0d5b3606a04e8f931d7f0ee73f9c7a87604
ramoslucas <lucasalbuquerque5412@gmail.com>
2021-05-15 21:50:23 +0100

    Js slide image

--- js/script.js
+++ js/script.js
@@ -204,12 +204,7 @@ var total_drink=3;
         item.addEventListener("keyup", CalculateItemsDrinkValue)
     })    
 
-    
-    
-    
-    
-    
-    
+
     
     function CalculateTotalBill(){
       
@@ -219,3 +214,35 @@ var total_drink=3;
     
     
 
+    let time = 3000,
+
+    currentImageIndex = 0,
+    images = document.querySelectorAll ("#slider img")
+    max = images.length;
+    
+    
+    function nextImage() {
+    
+      images[currentImageIndex].classList.remove("selected")
+    
+      currentImageIndex++;
+    
+      if (currentImageIndex >= max) 
+      currentImageIndex = 0;
+    
+      images[currentImageIndex].classList.add("selected")
+    
+    }
+    
+    
+    function start () {
+    
+      setInterval(() => {
+    
+        //troca de imagem
+        nextImage()
+    
+      }, time)
+    }
+    
+    window.addEventListener("load", start)
